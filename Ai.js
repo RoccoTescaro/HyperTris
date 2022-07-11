bebd6cc9c7bb6083ad04d8f5f3836126e650858f
Rocco Tescaro <90927550+RoccoTescaro@users.noreply.github.com>
2022-07-11 05:01:51 +0200

    Add files via upload

---
+++ Ai.js
@@ -0,0 +1,78 @@
+onmessage = function(e) {
+  const result = findBestMove(e.data[0],7);
+  postMessage(workerResult);
+}
+
+
+function principalVariationSearch(board_, depth_, alpha_=-9999999, beta_=9999999)
+{
+  if(depth_ == 0)
+    return board_.turn*board_.evaluate();
+
+  var checkResult = board_.check(0,board_.chunks);
+  if( checkResult != 0)
+    return -9999999*(checkResult != 2); //return 0 if stalemate else 9999999
+
+  var possibleMoves = board_.generatePossibleMoves();
+  order(possibleMoves);
+
+  for(var moveIndex = 0; moveIndex < possibleMoves.length; ++moveIndex)
+  {
+    var move = possibleMoves[moveIndex];
+    var score;
+    board_.makeMove(move);
+
+    if(moveIndex == 0)
+        score = -principalVariationSearch(board_, depth_ - 1, -beta_, -alpha_);
+    else
+    {
+        score = -principalVariationSearch(board_, depth_ - 1, -alpha_ - 1, -alpha_);
+
+        if(alpha_ < score && score < beta_)
+          score = -principalVariationSearch(board_, depth_ - 1, -beta_, -score);
+    }
+
+    board_.undoMove();
+
+    alpha_ = Math.max(alpha_, score);
+    if(alpha_ >= beta_)
+      break;
+  }
+
+  return alpha_;
+}
+
+function findBestMove(board_, depth_)
+{
+  var bestScore = -9999999;
+  var bestMove = undefined;
+
+  var possibleMoves = board_.generatePossibleMoves();
+  order(possibleMoves);
+
+  for(var moveIndex = 0; moveIndex < possibleMoves.length; ++moveIndex)
+  {
+    var move = possibleMoves[moveIndex];
+    board_.makeMove(move);
+    var score = -principalVariationSearch(board_,depth_);
+    board_.undoMove();
+    if(score > bestScore)
+    {
+      bestScore = score;
+      bestMove = move;
+    }
+
+    //console.log(move,score);
+  }
+
+  return [bestMove,bestScore];
+
+}
+
+function order(possibleMoves_){
+  var eval = [1,0,1,0,2,0,1,0,1];
+  possibleMoves_.sort((a,b) =>
+  {
+    return eval[b%9]-eval[a%9];
+  });
+}
